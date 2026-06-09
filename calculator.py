be182aebe49203c597968e03bbdcc3d99cd53408
manthanrathi10-code <manthanrathi10@gmail.com>
2026-06-09 11:55:24 +0530

    Added calculation history

---
+++ calculator.py
@@ -0,0 +1,25 @@
+history = []
+
+num1 = float(input("Enter first number: "))
+op = input("Enter operator (+,-,*,/): ")
+num2 = float(input("Enter second number: "))
+
+if op == "+":
+    result = num1 + num2
+elif op == "-":
+    result = num1 - num2
+elif op == "*":
+    result = num1 * num2
+elif op == "/":
+    result = num1 / num2
+else:
+    result = "Invalid"
+
+print(result)
+
+history.append(result)
+print("History:", history)[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[A[B[B
+[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+
+
+[A[A
